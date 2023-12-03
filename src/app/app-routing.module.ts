eb9192bfb96f781a6f3643a940a0b6b5301be3d5
Somasekaran Dhanapal <somasekaran.dhanapal@samco.in>
2023-12-03 16:57:53 +0530

    header route highlighted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CompanyComponent } from './company/company.component';
+import { TechnologyComponent } from './technology/technology.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'company',component:CompanyComponent},
+  {path:'technology',component:TechnologyComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  constructor(private route:Router){
+
+  }
+
+  ngOnInit(){
+      console.log("debug act route ")
+      let url = location.href;
+      this.selectTabBasedOnRoute(url);
+  }
+
+  selected = "";
+
+  selectTabBasedOnRoute(url:any){
+    if(url.includes('home')){
+      this.navto('home')
+    }else if(url.includes('company')){
+      this.navto('company')
+    }else if(url.includes('technology')){
+      this.navto('technology')
+    }
+
+  }
+
+  navto(comp:any){
+      this.selected = comp?.toLowerCase();
+      console.log("debug selected",this.selected)
+      
+      if(comp == "Home"){
+        this.route.navigateByUrl("/home")
+      }else if(comp == "Company"){
+        this.route.navigateByUrl("/company")
+      }else if(comp == "Technology"){
+        this.route.navigateByUrl("/technology")
+      }
+  }
+  
+
 
 }
