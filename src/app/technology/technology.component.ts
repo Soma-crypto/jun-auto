cfb0eb14a008b8771d427c28776449ed2c5b0f21
Somasekaran Dhanapal <somasekaran.dhanapal@samco.in>
2024-02-04 18:18:19 +0530

    showing dynamic content

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -7,5 +7,49 @@ import { Component } from '@angular/core';
 })
 export class TechnologyComponent {
   length = 10;
+  selectedIndex = 1;
+  carousal = [
+    {
+      imagesRow1:[{img:'creta.jpeg'},{img:'alcasar.jpeg'}],
+      imagesRow2:[{img:'creta.jpeg'},{img:'alcasar.jpeg'}],
+      contentHeading:'Lorem Ipsum',
+      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+      focusId:"content1",
+      index:1,
+    },
+    {
+      imagesRow1:[{img:'creta.jpeg'},{img:'alcasar.jpeg'}],
+      imagesRow2:[{img:'creta.jpeg'},{img:'alcasar.jpeg'}],
+      contentHeading:'Ipsum Lorem',
+      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+      focusId:"content1",
+      index:2,
+    },
+    {
+      imagesRow1:[{img:'creta.jpeg'},{img:'alcasar.jpeg'}],
+      imagesRow2:[{img:'creta.jpeg'},{img:'alcasar.jpeg'}],
+      contentHeading:'Lorem dupsam',
+      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+      focusId:"content1",
+      index:3,
+    },
+    {
+      imagesRow1:[{img:'creta.jpeg'},{img:'alcasar.jpeg'}],
+      imagesRow2:[{img:'creta.jpeg'},{img:'alcasar.jpeg'}],
+      contentHeading:'dupsam Lorem Ipsum',
+      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+      focusId:"content1",
+      index:4,
+    }
+  ]
+
+  ngOnInit(){
+
+  }
+
+  moveCarousalTo(idx:any){
+    console.log(idx)
+    this.selectedIndex = idx;
+  }
 
 }
